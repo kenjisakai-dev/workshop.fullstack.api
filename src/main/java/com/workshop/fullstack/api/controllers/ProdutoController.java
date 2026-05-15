bd95c7893c800142b905c91144be4acf5a7f2951
Kenji <kenjisakai80@gmail.com>
2026-05-15 17:13:00 -0300

    implementação do cors

--- src/main/java/com/workshop/fullstack/api/controllers/ProdutoController.java
+++ src/main/java/com/workshop/fullstack/api/controllers/ProdutoController.java
@@ -3,6 +3,7 @@ package com.workshop.fullstack.api.controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -14,6 +15,7 @@ import com.workshop.fullstack.api.services.ProdutoService;
 
 @RestController
 @RequestMapping("/api/produtos")
+@CrossOrigin(origins = "*")
 public class ProdutoController {
     @Autowired
     private ProdutoService service;
